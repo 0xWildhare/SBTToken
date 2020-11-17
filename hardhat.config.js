ff99d60bbf112aabe9b34943f2241e2fa717e363
Wildhare1979 <sschell1979@gmail.com>
2020-11-16 21:23:42 -0800

    deployed on Ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+require('dotenv').config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.6.2",
+  networks: {
+    ropsten: {
+      url: process.env.URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    localhost: {
+      url: "http://localhost:9111",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+
 };
